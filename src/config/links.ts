5f9d4110afdad63c78e9bac4de20bd6031c5403e
Max Maeder <max.s.maeder@gmail.com>
2023-08-13 23:18:12 +0200

    Move member links to config

--- src/config/links.ts
+++ src/config/links.ts
@@ -11,6 +11,17 @@ const SOCIAL_LINKS = [
   },
 ];
 
+const MEMBER_LINKS = [
+  {
+    label: "Register on WIN",
+    href: "https://win.wisc.edu/organization/leetcode-uw"
+  },
+  {
+    label: "Attendance form",
+    href: "https://forms.gle/XGu5ikFZKBnbiExh6"
+  },
+];
+
 const MAILCHIMP_URL = "https://leetcodeclub.us21.list-manage.com/subscribe/post?u=53d0a5023cce7f310e6bfa082&amp;id=095bca25cb&amp;f_id=007654e1f0";
 
-export { SOCIAL_LINKS, MAILCHIMP_URL };
+export { SOCIAL_LINKS, MEMBER_LINKS, MAILCHIMP_URL };
